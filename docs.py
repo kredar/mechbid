5bc070107a2aac789511c813993839ac2399f4e2
Alex <alex.keselman@gmail.com>
2013-09-16 01:00:13 -0400

    reverted back the changes caused by "Merge remote-tracking branch origin/master"

--- docs.py
+++ docs.py
@@ -133,27 +133,26 @@ class BaseDocumentManager():
             loc_expr = 'distance(location, geopoint(%s, %s)) < 50000' % (exampleLat, exampleLon)
 
             #expression='name',
-            # subject_desc = search.SortExpression(
-            #     expression=loc_expr,
-            #     direction=search.SortExpression.ASCENDING,
-            #     default_value='')
+            subject_desc = search.SortExpression(
+                expression=loc_expr,
+                direction=search.SortExpression.ASCENDING,
+                default_value='')
 
             # Sort up to 1000 matching results by subject in descending order
-            #sort = search.SortOptions(expressions=[subject_desc], limit=1000)
+            sort = search.SortOptions(expressions=[subject_desc], limit=1000)
 
             # Set query options
             options = search.QueryOptions(
                 limit=limit, # the number of results to return
                 cursor=cursor,
-            #    sort_options=sort,
+                sort_options=sort,
                 returned_fields=['name', 'address', 'location', 'pid', 'brands', 'phones'],
                 snippeted_fields=['content'])
 
             #ALEXK added location example here, remove if all works
             # query = "distance(store_location, geopoint(-33.857, 151.215)) < 4500"
 
-            query = search.Query(query_string=query_string + ' distance(location, geopoint(%s, %s)) < 50000' %
-                                              (exampleLat, exampleLon), options=options)
+            query = search.Query(query_string=query_string, options=options)
             index = search.Index(name=INDEX_NAME)
 
             # Execute the query
